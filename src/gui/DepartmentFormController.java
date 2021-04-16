22f4708db28f6f098970f9a966b799002857b7b2
Ricarvalho87 <79373894+Ricarvalho87@users.noreply.github.com>
2021-04-17 00:10:44 +0100

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,27 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 	
 	
 	private Department entity;
+	
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
@@ -36,13 +45,34 @@ public class DepartmentFormController implements Initializable{
 	private Button btCancel;
 	
 	@FXML
-	private void onBtSaveAction() {
-		System.out.println("save");
+	private void onBtSaveAction(ActionEvent event) {
+		if(service==null) {
+			throw new IllegalStateException("Service was null");
+		}
+		if(entity==null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		try {
+		entity=getFormData(); 
+		service.saveOrUpdate(entity);
+		Utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
+	private Department getFormData() {
+		Department obj=new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+
 	@FXML
-	private void onBtcancelAction() {
-		System.out.println("cancel");
+	private void onBtcancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 		
 	}
 	
@@ -59,6 +89,12 @@ public class DepartmentFormController implements Initializable{
 	public void setDepartment(Department entity) {
 		this.entity=entity;
 	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service=service;
+	}
+	
+	
 	public void updateFormData() {
 		if(entity==null) {
 			throw new IllegalStateException("Entity was null");
